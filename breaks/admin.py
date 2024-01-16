f26a918330776ff53662d264e4d8230f1055495c
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-01-16 16:49:24 +0500

    change

--- breaks/admin.py
+++ breaks/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-
 from breaks.models import organisations, groups, replacements
+
+
+#########
+# INLINES
+#########
+class ReplacementEmployeeInline(admin.TabularInline):
+    model = replacements.ReplacementEmployee
+    fields = ('employee', 'status')
 
 
 @admin.register(organisations.Organisation)
@@ -13,9 +20,17 @@ class GroupAdmin(OrganisationAdmin):
     list_display = ('id', 'name', 'manager', 'min_active')
 
 
+@admin.register(replacements.ReplacementStatus)
+class ReplacementStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'code', 'name', 'sort', 'is_active'
+    )
+
+
 @admin.register(replacements.Replacement)
 class ReplacementAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'group', 'date', 'break_start', 'break_end', 'break_max_duration'
     )
 
+    inlines = [ReplacementEmployeeInline]
--- breaks/models/replacements.py
+++ breaks/models/replacements.py
@@ -4,6 +4,21 @@ from django.db import models
 User = get_user_model()
 
 
+class ReplacementStatus(models.Model):
+    code = models.CharField('Код', max_length=16, primary_key=True)
+    name = models.CharField('Название', max_length=32)
+    sort = models.PositiveSmallIntegerField('Сортировка', null=True, blank=True)
+    is_active = models.BooleanField('Активность', default=True)
+
+    class Meta:
+        verbose_name = 'Статус смены'
+        verbose_name_plural = 'Статусы смены'
+        ordering = ('sort',)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
 class Replacement(models.Model):
     group = models.ForeignKey(
         'breaks.Group', models.CASCADE, 'replacements',
@@ -19,8 +34,29 @@ class Replacement(models.Model):
     class Meta:
         verbose_name = 'Смена'
         verbose_name_plural = 'Смены'
-        ordering = ('date',)
+        ordering = ('-date',)
 
     def __str__(self):
         return f"Смена №{self.pk} для {self.group}"
 
+
+class ReplacementEmployee(models.Model):
+    employee = models.ForeignKey(
+        User, models.CASCADE,
+        verbose_name='Сотрудник'
+    )
+    replacement = models.ForeignKey(
+        'breaks.Replacement', models.CASCADE,
+        verbose_name='Смена'
+    )
+    status = models.ForeignKey(
+        'breaks.ReplacementStatus', models.RESTRICT, 'replacement_employees',
+        verbose_name='Статус'
+    )
+
+    class Meta:
+        verbose_name = 'Смена - Сотрудник'
+        verbose_name_plural = 'Смены - Сотрудники'
+
+    def __str__(self):
+        return f"Смена {self.replacement} для {self.employee}"
